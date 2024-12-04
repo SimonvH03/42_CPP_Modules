dfa3337b5252c62dbbbd76108840c688adbaee0a
simon <si.hoogstraaten86@gmail.com>
2024-12-04 19:11:21 +0100

    made main conditional for makemake.sh

--- makemake.sh
+++ makemake.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 # Ensure the script is called with the correct number of arguments
-if [ "$#" -ne 2 ]; then
-	echo "Usage: $0 <source_directory> <program_name>"
+if [ "$#" -lt 2 ]; then
+	echo "Usage: $0 <source_directory> <program_name> {add_main=0,1}"
 	exit 1
 fi
 
 # Assign arguments to variables
 SRC_DIR=$1
 PROGRAM_NAME=$2
+ADD_MAIN=${3:-0}
 
 # Find all .hpp and .cpp files in the current directory
-HDR_FILES=$(find $SRC_DIR -maxdepth 1 -name "*.hpp" -printf "%f \\\\\n\t\t\t")
-SRC_FILES=$(find $SRC_DIR -maxdepth 1 -name "*.cpp" -printf "%f \\\\\n\t\t\t")
+format_files()
+{
+    find "$SRC_DIR" -maxdepth 1 -name "*.$1" -exec basename {} \; | sort -u | awk '{
+        if (NR == 1) printf "%s", $0;
+        else printf " \\\n\t\t\t%s", $0;}'
+}
+
+HDR_FILES=$(format_files "hpp")
+SRC_FILES=$(format_files "cpp")
 
 # Create the Makefile
 cat > "$SRC_DIR/Makefile" <<EOF
@@ -42,14 +50,21 @@ EOF
 # Notify the user of the created file
 echo "Makefile created for program: $PROGRAM_NAME"
 
-# Create #include list for HDR files
-HDR_FILES=$(find $SRC_DIR -maxdepth 1 -name "*.hpp" -printf "#include \"%f\"\\n" | sort)
-# Create main.cpp
-cat > "$SRC_DIR/main.cpp" <<EOF
-$HDR_FILES
+# Conditionally create main.cpp if add_main is set to 1
+if [ "$ADD_MAIN" -eq 1 ]; then
+    # Generate #include list for HDR files
+    INCLUDES=$(find "$SRC_DIR" -maxdepth 1 -name "*.hpp" -printf "#include \"%f\"\\n" | sort)
+
+    # Create main.cpp
+    cat > "$SRC_DIR/main.cpp" <<EOF
+$INCLUDES
 
 int	main(void)
 {
 
 }
 EOF
+
+	# Notify the user of the created file
+    echo "main.cpp created #including headers: $(find "$SRC_DIR" -maxdepth 1 -name "*.hpp" -printf "%f " | sort)"
+fi
