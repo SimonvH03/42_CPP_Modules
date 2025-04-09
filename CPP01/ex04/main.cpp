cff4b032b6c2dee896ba67215a01075ed534e471
Simon <si.hoogstraaten86@gmail.com>
2025-04-09 23:35:04 +0200

    removed unnessecary files from ex00-01, reformatted ex04-06

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -22,16 +22,19 @@ int	main(int argc, char *argv[])
 {
 	if (argc != 4)
 		return error("Usage: \"./Sed is for losers\" filename s1 s2", EINVAL);
+
 	std::string filenameIn = argv[1];
 	std::string	filenameOut = filenameIn + ".replace";
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
 	std::ifstream fileIn(filenameIn.data());
+
 	if (!fileIn.is_open())
 		return error("Error: failed to open file", errno);
 	std::ofstream fileOut(filenameOut.data());
 	if (!fileOut.is_open())
 		return error("Error: failed to create file", errno);
+
 	std::string line;
 	while (std::getline(fileIn, line))
 	{
