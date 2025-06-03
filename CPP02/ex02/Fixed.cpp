76016f7aa83475b58e8c27cd1eb06a585198c8e4
Simon <svan-hoo@student.codam>
2025-06-03 20:47:07 +0200

    CPP02/ex02: added brackets around return values

--- CPP02/ex02/Fixed.cpp
+++ CPP02/ex02/Fixed.cpp
@@ -187,22 +187,22 @@ int		Fixed::getRawBits() const
 
 Fixed&	Fixed::min(Fixed &a, Fixed &b)
 {
-	return (a < b) ? a : b;
+	return ((a < b) ? a : b);
 }
 
 Fixed&	Fixed::max(Fixed &a, Fixed &b)
 {
-	return (a > b) ? a : b;
+	return ((a > b) ? a : b);
 }
 
 Fixed const&	Fixed::min(Fixed const &a, Fixed const &b)
 {
-	return (a < b) ? a : b;
+	return ((a < b) ? a : b);
 }
 
 Fixed const&	Fixed::max(Fixed const &a, Fixed const &b)
 {
-	return (a > b) ? a : b;
+	return ((a > b) ? a : b);
 }
 
 std::ostream	&operator<<(std::ostream &os, Fixed const &fixed)
