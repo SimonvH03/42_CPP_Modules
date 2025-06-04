5a77a2079cb22864c676b0766ed44314e8d8a04b
simon <si.hoogstraaten86@gmail.com>
2025-06-04 15:50:23 +0200

    set up CPP02/ex03 plan

--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -1,7 +1,17 @@
-#include "Fixed.hpp"
+#include "Point.hpp"
 #include <iostream>
 
 int	main(void)
 {
-	return (0);
+	Point const	a(0,0);
+	Point const	b(1,0);
+	Point const c(0,1);
+
+	Point const p(1,1);
+
+	// evaluate if P is on the same side of AB as C
+	// same for P and B with line AC, etc.
+	// if any fails, return (false), otherwise continue
+	// until three sides are checked
+	return (true);
 }
