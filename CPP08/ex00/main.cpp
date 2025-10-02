f6c6a6e9963916e92aada7fed4f97f7693d19f91
simon <si.hoogstraaten86@gmail.com>
2025-10-02 12:43:01 +0200

    CPP08/ex00: improved test

--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -3,12 +3,16 @@
 
 #include "easyfind.hpp"
 
+static constexpr std::size_t Size = 100;
+
 int main(void)
 {
-	const size_t		N = 3;
 	const int			needle = 42;
-	std::vector<int>	numbers(N, 0);
+	std::vector<int>	numbers(Size, 0);
 
+	std::srand(time(NULL));
+	for (size_t i = 0; i < Size; ++i)
+		numbers[i] = std::rand();
 	numbers.push_back(needle);
 	if (*easyfind(numbers, needle) == needle)
 		std::cout << "That was easy!\n";
